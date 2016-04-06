ab9d695fb7b0416bbe7323524826b839e2e68b9b
Rich Abdill <richard.abdill@gmail.com>
2016-04-05 22:29:25 -0500

    New in-game modifications are working, wow

--- bishops_map_new.js
+++ bishops_map_new.js
@@ -2,48 +2,24 @@ function goTo(target) {
 	console.log("GOING TO", target);
 	console.log(nodes);
 	var targetIndex = _.findIndex(nodes, {"id": target});
-	if(targetIndex === null) throw "goTo function invoked with non-existent node.";
+	console.log("targetIndex=", targetIndex);
+	if(targetIndex === -1) throw "goTo function invoked with non-existent node.";
 
 	currentLocation = target;
 	if(nodes[targetIndex].type === "room") processChanges(nodes[targetIndex]);
-	document.getElementById('description').innerHTML = nodes[targetIndex].entrance
+	current = nodes[targetIndex];
+	document.getElementById('description').innerHTML = current.entrance
 };
 
 function processChanges(node) {
-	//TODO
+	_(node.changes).forEach(function(f) {
+		console.log("CHANGING", f);
+		f();
+	});
 };
 
 function initialize() {
 	document.getElementById("gameName").innerHTML = game.settings.title;
-
-	// make sure all the default settings are configured:
-	var required = {
-		"allow text to speech" : false,
-		"allow speech to text" : false,
-		"print directions" : true,
-		"allow default synonyms" : true
-	};
-
-	var defaultSynonyms = {
-		"actions" : {
-			"take" : ["get","steal","t"],
-			"examine" : ["inspect","look","x"],
-			"look" : ["l"],
-			"smash" : ["break"],
-			"smell" : ["sniff"],
-			"taste" : ["lick"]
-		},
-		"items" : {
-			"photo" : ["picture"],
-			"north" : ["n"],
-			"east" : ["e"],
-			"south" : ["s"],
-			"west" : ["w"]
-		}
-	};
-
-	game.settings = _.assign({}, required, game.settings);
-
 	goTo(currentLocation);
 };
 
@@ -59,6 +35,9 @@ function processCommand(command) {
 	command = _.difference(command, articles);
 
 	console.log("COMMAND", command);
+	if(command[0] === "go") {
+		if(current.exits[command[1]]) goTo(current.exits[command[1]]);
+	}
 };
 
 // load everything up:
