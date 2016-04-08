29a846f0c9e8c662a467770e5dc4d153d01012ab
Rich Abdill <richard.abdill@gmail.com>
2016-04-07 21:12:06 -0500

    'with' article working better

--- bishops_map_new.js
+++ bishops_map_new.js
@@ -134,7 +134,6 @@ function parseSynonyms(item, verb) {
 function checkRoomCommands(c) {
 	var verb = c[0];
 	var noun = c[1] || false;
-	var indirect = c[2] || false
 
 	for(var i=0, item; item = (current.items || [])[i]; i++) {
 		if(!item.state) item.state = 'default';
@@ -154,7 +153,7 @@ function checkRoomCommands(c) {
 
 			if(item.states[verb]) {
 				if(item.states[verb].requires) {	// if it has requirements
-					var passes = item.states[verb].requires();
+					var passes = item.states[verb].requires(c);
 					if(typeof passes === 'string') { // rejected with message
 						printMessage(passes);
 						return true;	// it's settled; no need to keep searching
